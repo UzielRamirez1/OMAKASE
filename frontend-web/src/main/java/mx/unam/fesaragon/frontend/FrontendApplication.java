29c56be6433c46ebea60300dd08a3df512cdb8d3
Julio-Emmanuel <145365288+Julio-Emmanuel@users.noreply.github.com>
2026-05-20 20:39:49 -0600

    docker files listos front end
    ░░░░░░░█▐▓▓░████▄▄▄█▀▄▓▓▓▌█
    ░░░░░▄█▌▀▄▓▓▄▄▄▄▀▀▀▄▓▓▓▓▓▌█
    ░░░▄█▀▀▄▓█▓▓▓▓▓▓▓▓▓▓▓▓▀░▓▌█
    ░░█▀▄▓▓▓███▓▓▓███▓▓▓▄░░▄▓▐█▌
    ░█▌▓▓▓▀▀▓▓▓▓███▓▓▓▓▓▓▓▄▀▓▓▐█
    ▐█▐██▐░▄▓▓▓▓▓▀▄░▀▓▓▓▓▓▓▓▓▓▌█▌
    █▌███▓▓▓▓▓▓▓▓▐░░▄▓▓███▓▓▓▄▀▐█
    █▐█▓▀░░▀▓▓▓▓▓▓▓▓▓██████▓▓▓▓▐█
    ▌▓▄▌▀░▀░▐▀█▄▓▓██████████▓▓▓▌█▌
    ▌▓▓▓▄▄▀▀▓▓▓▀▓▓▓▓▓▓▓▓█▓█▓█▓▓▌█▌
    █▐▓▓▓▓▓▓▄▄▄▓▓▓▓▓▓█▓█▓█▓█▓▓▓▐█

---
+++ frontend-web/src/main/java/mx/unam/fesaragon/frontend/FrontendApplication.java
@@ -0,0 +1,11 @@
+package mx.unam.fesaragon.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+}
